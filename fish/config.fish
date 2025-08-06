2a0d5c67301044c2fbd6e23c803c31a9dec290e7
Dr-helicopter <hashmya1@gmail.com>
2025-08-06 09:17:13 +0330

    1.3 changes made to keybindings

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
 	abbr -a dr2 /mnt/drive2
 	abbr -a cd.. cd ..
 	abbr -a gh.getTOKEN set TOKEN $(gh auth token)
+	abbr -a wifi.scn nmcli device wifi rescan
+	abbr -a wifi.cnct nmcli device wifi connect
 end
 
 
@@ -9,6 +11,8 @@ end
 function control
     love ~/lua_apps/control_panel
 end
+
+
 
 
 function cls
@@ -24,4 +28,23 @@ function ifconf
 end
 
 
+function wifi.Home
+	nmcli device wifi rescan
+	sleep 2 
+	nmcli device wifi connect weeeee
+end
+
+function wifi.Phon
+	nmcli device wifi rescan
+	sleep 2 
+	nmcli device wifi connect Helic
+end
+
+
 set -Ux MANPAGER 'nvim +Man!'
+set -Ux EDITOR 'nvim'
+status --is-interactive; and source (pyenv init -|psub)
+
+
+# set keybindings
+source ~/.config/fish/keybindings.fish
